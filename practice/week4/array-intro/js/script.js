9689eeeca80908ef7679e8a8b58906d667ce5fb0
YeEun Kim <kye1115z@naver.com>
2024-09-17 16:46:55 -0700

    feat: array-intro 수업

---
+++ practice/week4/array-intro/js/script.js
@@ -0,0 +1,20 @@
+let weekdays = ["Wednesday"];
+
+// Adding Values
+weekdays.push("Thursday", "Friday"); // end
+weekdays.unshift("Monday", "Tuesday"); // start
+
+// Accessing Array Items
+
+// Deleting items
+weekdays.splice(1, 1, "Martes");
+console.log(weekdays);
+
+//  Randomizing an Array
+let shuffledWeekdays = _.shuffle(weekdays);
+console.group(shuffledWeekdays);
+
+// Looping
+for (let i of weekdays) {
+  console.log(i);
+}
