dbc312667b9bb3042ce9b706baf3e0a51752f824
YeEun Kim <kye1115z@naver.com>
2024-10-24 16:26:09 -0700

    week8 lab

--- labs/week8/index.mjs
+++ labs/week8/index.mjs
@@ -33,8 +33,55 @@ app.get("/saturn", (req, res) => {
 });
 
 app.get("/nasa", async (req, res) => {
-  let nasaData = await getNasaData();
+  const { month, day, year } = req.query;
+  const date = `${year}-${month}-${day}`;
+  console.log(date);
+  let nasaData = await getNasaData(date);
   res.render("nasa", { nasaData });
+});
+
+app.get("/planetInfo", async (req, res) => {
+  let body = req.query.selectedBody;
+  let bodyInfo = {
+    description: "",
+    image: "",
+  };
+
+  if (body === "Comets") {
+    bodyInfo.description = planets.getComets().def;
+    bodyInfo.image = planets.getComets().image;
+  } else if (body === "Asteriods") {
+    bodyInfo.description = planets.getAsteroids().def;
+    bodyInfo.image = planets.getAsteroids().image;
+  } else if (body === "Mercury") {
+    bodyInfo.description = planets.getMercury().description;
+    bodyInfo.image = planets.getMercury().image;
+  } else if (body === "Venus") {
+    bodyInfo.description = planets.getVenus().description;
+    bodyInfo.image = planets.getVenus().image;
+  } else if (body === "Earth") {
+    bodyInfo.description = planets.getEarth().description;
+    bodyInfo.image = planets.getEarth().image;
+  } else if (body === "Mars") {
+    bodyInfo.description = planets.getMars().description;
+    bodyInfo.image = planets.getMars().image;
+  } else if (body === "Jupiter") {
+    bodyInfo.description = planets.getJupiter().description;
+    bodyInfo.image = planets.getJupiter().image;
+  } else if (body === "Saturn") {
+    bodyInfo.description = planets.getSaturn().description;
+    bodyInfo.image = planets.getSaturn().image;
+  } else if (body === "Uranus") {
+    bodyInfo.description = planets.getUranus().description;
+    bodyInfo.image = planets.getUranus().image;
+  } else if (body === "Neptune") {
+    bodyInfo.description = planets.getNeptune().description;
+    bodyInfo.image = planets.getNeptune().image;
+  }
+
+  console.log(bodyInfo);
+
+  res.render("celestialBody", { celestialBodyInfo: bodyInfo });
 });
 
 app.listen(3000, () => {
@@ -54,14 +101,12 @@ async function randomImage() {
   }
 }
 
-async function getNasaData() {
-  let url = `https://api.nasa.gov/planetary/apod?api_key=9mUzIkhlZCZaOoMfspg7jMmwZCZ4LiRHtkgkambD&date=2024-10-10`;
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error("Error fetching image:", error);
-    return null;
-  }
+async function getNasaData(date) {
+  // date를 사용하여 API 호출 로직을 추가
+  const response = await fetch(
+    `https://api.nasa.gov/planetary/apod?&api_key=9mUzIkhlZCZaOoMfspg7jMmwZCZ4LiRHtkgkambD&date=${date}`
+  );
+  const data = await response.json();
+  console.log(data);
+  return data;
 }
