ef4dce4f713f5c3e7ba2decdd1b2feaad53d79e4
YeEun Kim <kye1115z@naver.com>
2024-11-08 02:16:55 -0800

    feat: week 11 lab

--- labs/week11/index.mjs
+++ labs/week11/index.mjs
@@ -5,6 +5,8 @@ const app = express();
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 const pool = mysql.createPool({
   host: "yeyeeun.online",
@@ -18,64 +20,65 @@ const conn = await pool.getConnection();
 
 //routes
 app.get("/", async (req, res) => {
-  let sql = `SELECT firstName, lastName, authorId FROM authors ORDER BY lastName`;
+  res.render("home");
+});
+
+app.get("/addAuthor", async (req, res) => {
+  // Like a link to the add author form
+  res.render("addAuthor");
+});
+
+app.post("/addAuthor", async (req, res) => {
+  // What happens when you submit a new author
+  let firstName = req.body.firstName;
+  let lastName = req.body.lastName;
+  let bio = req.body.biography;
+  let gender = req.body.gender;
+  let picture = req.body.picture;
+  let dob = req.body.dob;
+  let dod = req.body.dod;
+
+  console.log(firstName, lastName, bio, gender, picture, dob, dod);
+
+  let sql = `INSERT INTO authors
+              (firstName, lastName, biography)
+              VALUES (?, ?, ?)`;
+  console.log(firstName, lastName, bio);
+  let params = [firstName, lastName, bio];
+  const [rows] = await conn.query(sql, params);
+  console.log(rows);
+  res.render("addAuthor");
+});
+
+app.get("/addQuote", async (req, res) => {
+  let author_sql = `SELECT firstName, lastName, authorId FROM authors ORDER BY lastName`;
   let category_sql = `SELECT DISTINCT(category) FROM quotes`;
-  const [rows] = await conn.query(sql);
+  const [author_rows] = await conn.query(author_sql);
   const [categroy_rows] = await conn.query(category_sql);
-  res.render("home", { authors: rows, category: categroy_rows });
+  res.render("newQuote", { authors: author_rows, category: categroy_rows });
 });
 
-// api로 활용 가능
-app.get("/api/author/:authorId", async (req, res) => {
-  let authorId = req.params.authorId;
-  let sql = `SELECT * FROM authors WHERE authorId = ?`;
-  let sqlParams = [authorId];
-  const [rows] = await conn.query(sql, sqlParams);
-  res.send(rows);
+app.post("/addQuote", async (req, res) => {
+  // What happens when you submit a new author
+  let quote = req.body.quote;
+  let authorId = req.body.author;
+  let category = req.body.category;
+
+  let sql = `INSERT INTO quotes
+              (quote, authorId, category)
+              VALUES (?, ?, ?)`;
+  let params = [quote, authorId, category];
+  const [rows] = await conn.query(sql, params);
+  console.log(rows);
+  res.render("addAuthor");
 });
 
-app.get("/searchByKeyword", async (req, res) => {
-  let keyword = req.query.keyword;
-  let sql =
-    "SELECT `authorId`, `firstName`, `lastName`, `quote` FROM `quotes` NATURAL JOIN authors WHERE quote LIKE ?";
-  let sqlParams = [`%${keyword}%`];
-  const [rows] = await conn.query(sql, sqlParams);
-  res.render("quote.ejs", { rows: rows });
-});
-
-app.get("/searchByAuthor", async (req, res) => {
-  let author = req.query.author;
-  let sql =
-    "SELECT `firstName`, `lastName`, `quote` FROM `quotes` NATURAL JOIN authors WHERE authorId = ?";
-  let sqlParams = [author];
-  const [rows] = await conn.query(sql, sqlParams);
-
-  res.render("quote.ejs", { rows: rows });
-});
-
-app.get("/searchByCategory", async (req, res) => {
-  let category = req.query.category;
-  let sql = `
-    SELECT authors.firstName, authors.lastName, quotes.quote 
-    FROM quotes 
-    NATURAL JOIN authors 
-    WHERE category = ?`;
-  let sqlParams = [category];
-  const [rows] = await conn.query(sql, sqlParams);
-  res.render("quote.ejs", { rows: rows });
-});
-
-app.get("/searchByLikes", async (req, res) => {
-  let min = req.query.min;
-  let max = req.query.max;
-  let sql = `
-    SELECT authors.firstName, authors.lastName, quotes.quote 
-    FROM quotes 
-    NATURAL JOIN authors 
-    WHERE quotes.likes >= ? AND quotes.likes <= ?`;
-  let sqlParams = [min, max];
-  const [rows] = await conn.query(sql, sqlParams);
-  res.render("quote.ejs", { rows: rows });
+app.get("/Lists", async (req, res) => {
+  let author_sql = `SELECT firstName, lastName, authorId FROM authors ORDER BY lastName`;
+  let quote_sql = `SELECT quote, category FROM quotes`;
+  const [author_rows] = await conn.query(author_sql);
+  const [quote_rows] = await conn.query(quote_sql);
+  res.render("lists", { quote: quote_rows, authors: author_rows });
 });
 
 app.get("/dbTest", async (req, res) => {
@@ -84,6 +87,6 @@ app.get("/dbTest", async (req, res) => {
   res.send(rows);
 }); //dbTest
 
-app.listen(10040, () => {
+app.listen(3001, () => {
   console.log("Express server running");
 });
